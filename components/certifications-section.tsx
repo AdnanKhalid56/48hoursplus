ea5c78084e0d98b45fc2361c3664cafa037bf6e5
AdnanKhalid56 <adnankhaliduet.786fivv@gmail.com>
2025-08-09 09:17:36 +0500

    Update certifications-section.tsx
    contact changed

--- components/certifications-section.tsx
+++ components/certifications-section.tsx
@@ -255,7 +255,7 @@ export function CertificationsSection() {
             className="bg-green-600 hover:bg-green-700 text-white text-lg px-8 py-4 h-auto transition-all duration-300 hover:scale-105"
             onClick={() => {
               window.open(
-                "https://wa.me/9231944059354?text=I want to know more about 48hoursplus quality certifications and place an order",
+                "https://wa.me/923194405935?text=I want to know more about 48hoursplus quality certifications and place an order",
                 "_blank",
               )
             }}
